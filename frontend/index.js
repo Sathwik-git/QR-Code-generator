6ccf4d05a11ba50453474df0d6c3ac59ac3067de
Sathwik-git <sathwikbodakuntla041@gmail.com>
2024-07-25 18:52:06 +0530

    added frontend

---
+++ frontend/index.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const qrcode = document.getElementById("qrcode");
+
+button.addEventListener('click',myfunction);
+
+function myfunction(){
+    if(!input.value){
+        console.log("ooooo");
+    }else{
+        fetch('http://localhost:3000/',{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'link':`${input.value}`})
+        })
+        .then(res => res.json())
+        .then(data => {
+            qrcode.innerHTML = `${data.qr}`;
+        })
+        .catch(err=> console.log(`error: ${err}`));
+    }
+
+}
+
+
+
