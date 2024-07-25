6e1d7a0571a739b11c6ec7caa26f708ad5005621
Sathwik-git <sathwikbodakuntla041@gmail.com>
2024-07-25 19:01:00 +0530

    added backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const qrcode = require('qrcode')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/',async(req,res)=>{
+    const body = req.body;
+    const link = body.link;
+    const url = await qrcode.toDataURL(link)
+    res.status(200).json({'qr': `<img src="${url}"></img>`});
+})
+
+
+app.listen(port,()=>{console.log(`server started at ${port}`)});
