82b810f4f19a829efffac88a52173483ee3c8e1e
Sathwik-git <sathwikbodakuntla041@gmail.com>
2024-07-25 21:17:48 +0530

    changes few issues

--- frontend/index.js
+++ frontend/index.js
@@ -8,7 +8,7 @@ function myfunction(){
     if(!input.value){
         console.log("ooooo");
     }else{
-        fetch('https://qr-code-generator-cxim.vercel.app',{
+        fetch('http://localhost:3000/',{
             method:'POST',
             headers: {
                 'Content-Type': 'application/json',
